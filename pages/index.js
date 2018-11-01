81ce1b8951da0913dd92b72aa2cbf05c0d88af91
Christian Fleschhut <weltrekordhalter@gmail.com>
2018-11-01 19:52:03 +0100

    Style components w/ styled-jsx

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,49 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
 
+const PostLink = ({ show }) => (
+  <li key={show.id}>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 const Index = props => (
-  <div>
-    <Layout>
-      <h1>Batman TV Shows</h1>
-      <ul>
-        {props.shows.map(({ show }) => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  </div>
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({ show }) => (
+        <PostLink key={show.id} show={show} />
+      ))}
+    </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </Layout>
 );
 
 Index.getInitialProps = async () => {
--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,46 @@
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 const Post = props => (
   <Layout>
     <h1>{props.show.name}</h1>
     <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
     <img src={props.show.image.medium} />
+
+    <div className="markdown">
+      <Markdown
+        source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}
+      />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: 'Arial';
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: 0.6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
